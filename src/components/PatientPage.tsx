3b12dcb79afa55a7019c72607c6401cb136bc1d9
Jukka Stenlund <jukka@stenlund.fi>
2024-04-17 12:30:53 +0300

    Extended the listing on the patient's page to include the Entry's details. Started using some Material UI icons in the page. (Exercise 9.25: Patientor, step 6)

--- src/components/PatientPage.tsx
+++ src/components/PatientPage.tsx
@@ -1,14 +1,72 @@
 import { useEffect, useState } from "react";
-import { Patient } from "../types";
+import { Gender, Patient } from "../types";
 import { useParams } from 'react-router-dom';
 import patientService from "../services/patients";
-import { Diagnosis } from "../types";
+import { Diagnosis, 
+  Entry, 
+  HospitalEntry, 
+  OccupationalHealthcareEntry,
+  HealthCheckEntry } from "../types";
 
-interface Props {
+import WorkIcon from '@mui/icons-material/Work';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from '@mui/icons-material/Female';
+import HealthRatingBar from "./HealthRatingBar";
+
+
+interface PatientPageProps {
   diagnoses: Diagnosis[]
 }
 
-const PatientPage = ({diagnoses}: Props) => {
+const HospitalEntryDetails = ({entry}: {entry: HospitalEntry}) => (
+  <div>
+    <div>discharge: {entry.discharge.date} {entry.discharge.criteria}</div>
+  </div>
+);
+
+const OccupationalHealthcareEntryDetails = 
+  ({entry}: {entry: OccupationalHealthcareEntry}) => (
+  <div>
+    {entry.sickLeave && (
+      <div>sickleave from {entry.sickLeave.startDate} to {entry.sickLeave.endDate}</div>
+    )}
+  </div>
+);
+
+const HealthCheckEntryDetails =
+  ({entry}: {entry: HealthCheckEntry}) => {
+  const rating:number = entry.healthCheckRating;
+  return (
+    <HealthRatingBar rating={rating} showText={false}/>
+  );
+};
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled entry type: ${JSON.stringify(value)}`
+  );
+};
+
+interface EntryDetailsProps {
+  entry: Entry
+}
+
+const EntryDetails = ({entry}: EntryDetailsProps ) => {
+  switch (entry.type) {
+    case "Hospital":
+      return <HospitalEntryDetails entry={entry} />;
+    case "OccupationalHealthcare":
+      return <OccupationalHealthcareEntryDetails entry={entry} />;
+    case "HealthCheck":
+      return <HealthCheckEntryDetails entry={entry}/>;
+    default:
+      return assertNever(entry);
+  }
+};
+
+const PatientPage = ({diagnoses}: PatientPageProps) => {
   const [patient, setPatient] = useState<Patient | null>(null);
 
   const id = useParams().id;
@@ -39,17 +97,36 @@ const PatientPage = ({diagnoses}: Props) => {
     }
   };
 
+  const entryStyle = {
+    marginTop: 5,
+    paddingTop: 5,
+    paddingLeft: 8,
+    border: 'solid',
+    borderWidth: 1,
+    paddingBottom: 5
+  };
+
   return (
     <div className="App">
-      <h2>{patient.name}</h2>
+      <h2>{patient.name}
+        {patient.gender===Gender.Male && <MaleIcon />}
+        {patient.gender===Gender.Female && <FemaleIcon />}
+      </h2>
       <div>gender: {patient.gender}</div>
       <div>ssn: {patient.ssn}</div>
       <div>date of birth: {patient.dateOfBirth}</div>
       <div>occupation: {patient.occupation}</div>
       <h3>entries</h3>
       {patient.entries.map(e => (
-        <div key={e.id}>
-          <div>{e.date} {e.description}</div>
+        <div key={e.id} style={entryStyle}>
+          <div>
+            {e.date}&nbsp;
+            {e.type==="HealthCheck" && <MonitorHeartIcon />}
+            {e.type==="Hospital" && <LocalHospitalIcon />}
+            {e.type==="OccupationalHealthcare" &&  <WorkIcon />}
+            {e.type==="OccupationalHealthcare" &&  ' ' + e.employerName}
+          </div>
+          <div><i>{e.description}</i></div>
           {e.diagnosisCodes && (
             <ul>
               {e.diagnosisCodes.map(dc => (
@@ -60,6 +137,8 @@ const PatientPage = ({diagnoses}: Props) => {
               ))}
             </ul>
           )}
+          <EntryDetails entry={e} />
+          <div>diagnose by {e.specialist}</div>
         </div>
       ))}
     </div>
